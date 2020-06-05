34f811f8b458158da65e75eddc334e809b45ed44
David Hutchison <dhutchis68516@gmail.com>
2020-06-05 15:36:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -108,8 +108,8 @@ class Student
     SQL
     binding.pry
     DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row[0])
-    end
+      self.new_from_db(row)
+    end.first
   end
 
   def self.all_students_in_grade_X
